fad2351a091b955527ba3edd61e3b604941f43c8
alumno <alumno@pc-candelap.uacj.mx>
2025-02-19 10:31:31 -0700

    [FINALIZA] mi primera app usando storyboard

--- mi_primera_app_storyboard/mi_primera_app_storyboard/ViewController.swift
+++ mi_primera_app_storyboard/mi_primera_app_storyboard/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var cajon_de_texro: UILabel!
+    
+    @IBOutlet weak var entrada_de_texto: UITextField!
+    
+    @IBAction func cuando_le_picamos(_ sender: Any) {
+        cajon_de_texro.text = entrada_de_texto.text
+        entrada_de_texto.text = ""
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Ya cargo la pantalla inicial")
         // Do any additional setup after loading the view.
     }
 
 
 }
-
